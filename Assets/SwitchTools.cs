74342744e08347ec19e840172930fa7ee2631746
Meha Magesh <44281943+1010meha@users.noreply.github.com>
2025-11-17 20:26:39 -0800

    Initial commit

---
+++ Assets/SwitchTools.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+
+public class SwitchTools : MonoBehaviour
+{
+    public Gesso crackGesso;
+    public Gesso overGesso;
+    public RemoveVarnish removeVarnish;
+    public RemovePigment removeGesso;
+    public PaintColor paintCrack;
+    public GameObject FluorecentCrack;
+    public GameObject UVPainting;
+    public GameObject UnderPainting;
+    public GameObject varnishSetup;
+    public GameObject buttons;
+    public GameObject paintingParent;
+
+
+    public static SwitchTools Instance;
+
+    public enum Tools { UseGesso, RemoveGesso, UsePaint, UseVarnishRemover, VisibleLight, UVLight, InfraredLight, VarnishSetup }
+    void Start()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }       
+        SetAllToFalse();
+
+    }
+
+    public void SetAllToFalse()
+    {
+        crackGesso.enabled = false;
+        overGesso.enabled = false;
+        removeVarnish.enabled = false;
+        removeGesso.enabled = false;
+        paintCrack.enabled = false;
+        FluorecentCrack.SetActive(false);
+        UVPainting.SetActive(false);
+        UnderPainting.SetActive(false);
+        varnishSetup.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UseGesso()
+    {
+        SetAllToFalse();
+        crackGesso.enabled = true;
+        overGesso.enabled = true;
+      
+    }
+
+    public void RemoveGesso()
+    {
+        SetAllToFalse();
+        removeGesso.enabled = true;
+    }
+
+    public void UsePaint()
+    {
+        SetAllToFalse();
+        paintCrack.enabled = true;
+    }
+
+    public void UseVarnishRemover()
+    {
+        SetAllToFalse();
+        removeVarnish.enabled = true;
+
+    }
+
+    public void VisibleLight()
+    {
+        SetAllToFalse();
+        FluorecentCrack.SetActive(true);
+    }
+    public void UVLight()
+    {
+        SetAllToFalse();
+        UVPainting.SetActive(true);
+    }
+    public void InfraredLight()
+    {
+        SetAllToFalse();
+        UnderPainting.SetActive(true);
+    }
+
+    public void GoToVarnishSetup()
+    {
+        SetAllToFalse();
+        buttons.SetActive(false);
+        paintingParent.SetActive(false);
+        varnishSetup.SetActive(true);
+
+    }
+}
